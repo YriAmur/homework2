4a3789350df310f65f2c18e03f17842cd7a66384
YriAmur <yr1979a@gmail.com>
2024-07-20 19:57:46 +0800

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+number_of_homework_completed = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_homework_completed
+
+print('Название курса -',course_name,'колличество выполненных ДЗ -',number_of_homework_completed,'количество затраченных часов -',number_of_hours_spent,'время на одно задание -',time_for_one_task)
